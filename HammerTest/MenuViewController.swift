571bdf3f623233e0970699f3587d5f3dc0ef0f42
Philip Gurin <gurinfilipp@gmail.com>
2021-07-09 23:59:00 +0300

    tableView data source and delegate done

--- HammerTest/MenuViewController.swift
+++ HammerTest/MenuViewController.swift
@@ -9,14 +9,34 @@ import UIKit
 import PinLayout
 
 class MenuViewController: UIViewController {
-
+    
     private var currentCity: String = "Москва ᐯV⌄"
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+       // tableView.backgroundColor = .yellow
+        tableView.tableFooterView = UIView()
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.addSubview(tableView)
         view.backgroundColor = .white
+        
+        tableView.dataSource = self
+        tableView.delegate = self
         setupNavigationBar()
+        setupTableView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.pin.all()
+    }
+    
+    private func setupTableView() {
+        
     }
     
     private func setupNavigationBar() {
@@ -24,13 +44,62 @@ class MenuViewController: UIViewController {
         navigationController?.navigationBar.tintColor = .black
         navigationController?.navigationBar.barTintColor = .systemGroupedBackground
         navigationController?.navigationBar.shadowImage = UIImage()
-    
     }
-
+    
     @objc
     private func cityButtonTapped() {
         print(#function)
     }
     
 }
+
+extension MenuViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 1
+        case 2:
+            return 10
+        default:
+            return 0
+        }
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            let cell = UITableViewCell()
+            cell.backgroundColor = .orange
+            return cell
+        case 1:
+            let cell = UITableViewCell()
+            cell.backgroundColor = .systemIndigo
+            return cell
+        default:
+            return .init()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 160
+        case 1:
+            return 60
+        default:
+            return 200
+        }
+    }
+    
+}
+
 
